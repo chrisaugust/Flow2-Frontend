c3486f1661246d14b520abe74931159572cf855c
Chris Danielson <christopher.a.danielson@protonmail.com>
2025-09-23 10:33:46 -0400

    Add Signup button on Login component; update api/services/api.js to not attach token for signup or login

--- src/services/api.js
+++ src/services/api.js
@@ -10,7 +10,9 @@ const api = axios.create({
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
-  if (token) {
+  
+  // Don't attach token for signup or login
+  if (token && !["/signup", "/login"].includes(config.url)) {
     config.headers.Authorization = `Bearer ${token}`;
   }
 
